2da45d7a9da1c35ee5f75d25b4bbd449c826118c
huangjuite <huangjuite@gmail.com>
2018-04-18 02:52:40 +0800

    update 4/18/2018

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,7 +1,57 @@
-
+import java.awt.Graphics;
 
 public abstract class GameObject {
-    protected int x,y;
+    protected int x;
+    protected int y;
+    protected ID id;
+    protected int volx;
+    protected int voly;
 
+    public GameObject(int x,int y,ID id){
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    public void setVolx(int volx) {
+        this.volx = volx;
+    }
+
+    public void setVoly(int voly) {
+        this.voly = voly;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    public int getVolx() {
+        return volx;
+    }
+
+    public int getVoly() {
+        return voly;
+    }
+    public abstract void tick();
+    public abstract void render(Graphics g);
 }
---
+++ src/Rocket.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Rocket extends GameObject {
+
+    public Rocket(int x, int y, ID id) {
+        super(x, y, id);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
